b423536a84536b9f8d149146fad3668c51d646fc
MahmoudGh01 <80069718+MahmoudGh01@users.noreply.github.com>
2026-03-13 04:50:27 +0100

    fix: set basename on BrowserRouter to match vite base config for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Home } from './pages/Home';
 function App(): React.JSX.Element {
   return (
     <MovieProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/m8-sheapit">
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
